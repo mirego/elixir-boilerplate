7145d188f8b27b484c6ab18df9fd82d1b84ec051
Mathieu Rheaume <mrheaume@mirego.com>
2017-12-05 15:58:24 -0500

    Favor Application.get_env instead of System.get_env during application startup

--- lib/phoenix_boilerplate/repo.ex
+++ lib/phoenix_boilerplate/repo.ex
@@ -6,6 +6,6 @@ defmodule PhoenixBoilerplate.Repo do
   DATABASE_URL environment variable.
   """
   def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+    {:ok, Keyword.put(opts, :url, Application.get_env(:phoenix_boilerplate, PhoenixBoilerplate.Repo)[:url])}
   end
 end
