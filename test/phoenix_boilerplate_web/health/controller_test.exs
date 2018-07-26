884234500f2deeed40f71cd4909a56b8a35aa87c
Rémi Prévost <remi@exomel.com>
2018-07-26 12:30:43 -0400

    Fix HealthController test

--- test/phoenix_boilerplate_web/health/controller_test.exs
+++ test/phoenix_boilerplate_web/health/controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhoenixBoilerplateWeb.Heatlh.ControllerTest do
 
   test "GET /health", %{conn: conn} do
     conn = get(conn, "/health")
-    assert text_response(conn, 200) == "ok"
+    assert text_response(conn, 200) == "The system looks OK."
   end
 end
