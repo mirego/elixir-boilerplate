c2b31d460269692bf808125829385aeca36b637c
Rémi Prévost <remi@exomel.com>
2019-03-21 17:53:48 -0400

    Add empty dummy data setup

---
+++ priv/repo/dummy.exs
@@ -0,0 +1,5 @@
+# Script for populating the database with disposable, development-oriented data. You can run it as:
+#
+#     mix run priv/repo/dummy.exs
+#
+# We recommend using test factories here, since they already provide good sample data.
