fe27ccb6d72ef59abaec35208338300acf3620b9
Pascal Hamel <phamel@crowdbase.com>
2019-04-11 09:59:27 -0400

    Fix lint

--- test/elixir_boilerplate_web/health/plug_test.exs
+++ test/elixir_boilerplate_web/health/plug_test.exs
@@ -3,9 +3,10 @@ defmodule ElixirBoilerplateWeb.Health.PlugTest do
 
   test "GET /health", %{conn: conn} do
     conn = get(conn, "/health")
+
     assert json_response(conn, 200) == %{
-      "status" => "ok",
-      "version" => Application.spec(:elixir_boilerplate, :vsn)
-    }
+             "status" => "ok",
+             "version" => Application.spec(:elixir_boilerplate, :vsn)
+           }
   end
 end
