e5ddaa1f97fc57524247d5cc51a171bc626e6d11
Rémi Prévost <remi@exomel.com>
2019-05-26 20:51:21 -0400

    Remove redundant comments from configuration files

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,9 @@
 use Mix.Config
 
-# Configure Phoenix endpoint
 config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
-# Configure Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   level: :info,
