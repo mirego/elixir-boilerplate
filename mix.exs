54b8c5cc9e12616c9f769291c2d30f68fc5b5ee8
Rémi Prévost <remi@exomel.com>
2019-06-18 13:32:58 -0400

    Add “mix check.unused_locked_deps” task

--- mix.exs
+++ mix.exs
@@ -78,7 +78,7 @@ defmodule ElixirBoilerplate.Mixfile do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
-      "compile.app": ["erlang.check_version", "compile.app"]
+      "compile.app": ["check.erlang_version", "compile.app"]
     ]
   end
 
