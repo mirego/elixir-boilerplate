e3bbd2dcdfa6e6fbcb894743498820fb84516580
Guillaume Cauchon <gcauchon@gmail.com>
2019-07-24 15:37:54 -0400

    Add missing ENV for Travis

--- config/test.exs
+++ config/test.exs
@@ -3,9 +3,6 @@ import Config
 config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
   http: [port: 4001],
   server: false,
-  secret_key_base: "G0ieeRljoXGzSDPRrYc2q4ADyNHCwxNOkw7YpPNMa+JgP9iGgJKT4K96Bw/Mf/pd",
-  session_key: "test",
-  signing_salt: "qh+vmMHsOqcjKF3TSSIsghwt2go48m2+IQ+kMTOB3BrSysSr7D4a21uAtt4yp4wn",
   static_url: [
     scheme: "https",
     host: "example.com",
