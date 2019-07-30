39f530456b06d1ff275ef6907a0653d45d921298
Rémi Prévost <remi@exomel.com>
2019-07-30 17:26:46 -0400

    Improve Content-Security-Policy value

--- lib/elixir_boilerplate_web/router.ex
+++ lib/elixir_boilerplate_web/router.ex
@@ -1,8 +1,6 @@
 defmodule ElixirBoilerplateWeb.Router do
   use Phoenix.Router
 
-  @secure_headers %{"content-security-policy" => "default-src 'self'"}
-
   pipeline :api do
     plug(:accepts, ["json"])
   end
@@ -12,7 +10,8 @@ defmodule ElixirBoilerplateWeb.Router do
     plug(:fetch_session)
     plug(:fetch_flash)
     plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers, @secure_headers)
+    plug(:put_secure_browser_headers)
+    plug(:put_content_security_policy)
     plug(:put_layout, {ElixirBoilerplateWeb.Layouts.View, :app})
   end
 
@@ -20,5 +19,22 @@ defmodule ElixirBoilerplateWeb.Router do
     pipe_through(:browser)
 
     get("/", Home.Controller, :index, as: :home)
+  end
+
+  def put_content_security_policy(conn, _) do
+    content_security_policy =
+      Mix.env()
+      |> content_security_policy()
+      |> Enum.join("; ")
+
+    put_resp_header(conn, "content-security-policy", content_security_policy)
+  end
+
+  defp content_security_policy(:dev) do
+    ["default-src 'self'", "script-src 'self' 'unsafe-eval' 'unsafe-inline'", "style-src 'self' 'unsafe-inline'"]
+  end
+
+  defp content_security_policy(_) do
+    ["default-src 'self'"]
   end
 end
