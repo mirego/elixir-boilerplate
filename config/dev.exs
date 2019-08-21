4a64ed79375ec754b9428c17a9aa51d962cd7b4e
Rémi Prévost <remi@exomel.com>
2019-08-21 19:05:29 -0400

    Add a working content security policy

--- config/dev.exs
+++ config/dev.exs
@@ -21,6 +21,8 @@ config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
     ]
   ]
 
+config :elixir_boilerplate, ElixirBoilerplateWeb.ContentSecurityPolicy, allow_unsafe_scripts: true
+
 config :logger, :console, format: "[$level] $message\n"
 
 config :phoenix, :stacktrace_depth, 20
