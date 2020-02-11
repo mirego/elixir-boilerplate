26df9ff2f9096c739d14ede4fb94d32813cb02cc
alarochelle <alarochelle@mirego.com>
2020-02-11 14:38:56 -0500

    Run formatter on test.exs

--- config/test.exs
+++ config/test.exs
@@ -24,5 +24,4 @@ config :logger, level: :warn
 
 config :elixir_boilerplate, ElixirBoilerplate.Repo, pool: Ecto.Adapters.SQL.Sandbox
 
-config :elixir_boilerplate, ElixirBoilerplate.Repo,
-  url: Environment.local_database_url?("DATABASE_URL")
+config :elixir_boilerplate, ElixirBoilerplate.Repo, url: Environment.local_database_url?("DATABASE_URL")
