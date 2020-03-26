bf91fcbd84f126ef9aa1d106ec9077e2502a411e
Mathieu Cote <mcote@mirego.com>
2020-03-26 15:26:47 -0400

    Add a deeper check for DB URL to enforce running against a test DB

--- config/test.exs
+++ config/test.exs
@@ -3,6 +3,19 @@ import Config
 # Import runtime configuration
 import_config "releases.exs"
 
+defmodule TestEnvironment do
+  @database_name_suffix "_test"
+
+  def enforce_test_database_name(key) do
+    url = Environment.get_local_url(key)
+
+    case String.contains?(url, @database_name_suffix) do
+      true -> url
+      _ -> raise "Expected database URL to ends with '#{@database_name_suffix}', got: #{url}"
+    end
+  end
+end
+
 config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
   http: [port: 4001],
   server: false,
@@ -24,4 +37,4 @@ config :logger, level: :warn
 
 config :elixir_boilerplate, ElixirBoilerplate.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
-  url: Environment.get_local_url("DATABASE_URL")
+  url: TestEnvironment.enforce_test_database_name("DATABASE_URL")
