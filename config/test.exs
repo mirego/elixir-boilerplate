c23d07136d06e5b514f120a55e931101432833e7
Guillaume Cauchon <gcauchon@gmail.com>
2020-10-13 22:41:17 -0400

    Modify test setup to work with runtime configuration

--- config/test.exs
+++ config/test.exs
@@ -14,27 +14,10 @@ defmodule TestEnvironment do
   end
 end
 
-config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
-  http: [port: 4001],
-  server: false,
-  secret_key_base: "G0ieeRljoXGzSDPRrYc2q4ADyNHCwxNOkw7YpPNMa+JgP9iGgJKT4K96Bw/Mf/pd",
-  static_url: [
-    scheme: "https",
-    host: "example.com",
-    port: "443"
-  ],
-  url: [
-    scheme: "https",
-    host: "example.",
-    port: "443"
-  ]
-
-config :elixir_boilerplate, ElixirBoilerplateWeb.Router,
-  session_key: "test",
-  session_signing_salt: "qh+vmMHsOqcjKF3TSSIsghwt2go48m2+IQ+kMTOB3BrSysSr7D4a21uAtt4yp4wn"
-
-config :logger, level: :warn
-
 config :elixir_boilerplate, ElixirBoilerplate.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   url: TestEnvironment.get_database_url()
+
+config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint, server: false
+
+config :logger, level: :warn
