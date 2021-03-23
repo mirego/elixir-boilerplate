2415dec3b121d49cad76a066ccccc4c018afca73
Rémi Prévost <rprevost@mirego.com>
2021-03-23 13:57:22 -0400

    Generate and expose JS source maps by default (#142)

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -8,10 +8,15 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = (_env, _options) => ({
+const devtoolOption = (mode) => {
+  if (mode === 'production') return 'source-map';
+  return 'eval';
+};
+
+module.exports = (_, {mode}) => ({
   optimization: {
     minimizer: [
-      new TerserPlugin({cache: true, parallel: true, sourceMap: false}),
+      new TerserPlugin({cache: true, parallel: true}),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
@@ -40,5 +45,6 @@ module.exports = (_env, _options) => ({
   plugins: [
     new MiniCssExtractPlugin({filename: '../css/app.css'}),
     new CopyWebpackPlugin({patterns: [{from: 'static/', to: '../'}]})
-  ]
+  ],
+  devtool: devtoolOption(mode)
 });
