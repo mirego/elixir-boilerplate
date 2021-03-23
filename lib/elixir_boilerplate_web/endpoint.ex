f2523bd6a702b0e173384d8e573df15c1665773c
Rémi Prévost <rprevost@mirego.com>
2021-03-23 13:35:54 -0400

    Nevermind 🤦‍♂️

--- lib/elixir_boilerplate_web/endpoint.ex
+++ lib/elixir_boilerplate_web/endpoint.ex
@@ -22,7 +22,7 @@ defmodule ElixirBoilerplateWeb.Endpoint do
     at: "/",
     from: :elixir_boilerplate,
     gzip: false,
-    only: ~w(css fonts images js map favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt)
   )
 
   # Code reloading can be explicitly enabled under the
