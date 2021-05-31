94821637b6c298825be64759d4b58432451f44aa
Rémi Prévost <rprevost@mirego.com>
2021-05-31 09:21:35 -0400

    Make test suite fail on compiler warning (#151)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,6 @@
+# NOTE: When using Elixir 1.12+, we could ditch the next line and use `mix test --warnings-as-errors` instead
+Code.put_compiler_option(:warnings_as_errors, true)
+
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 
 ExUnit.start()
