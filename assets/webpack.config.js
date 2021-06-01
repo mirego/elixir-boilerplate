caa4ff3266e573b587a33f00a5eb23b53442b7b7
Guillaume Cauchon <gcauchon@gmail.com>
2021-05-31 21:03:23 -0400

    Update npm dependencies/config for Webpack 5

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,30 +2,22 @@
 const path = require('path');
 const glob = require('glob');
 
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = (_env, options) => {
   const devMode = options.mode !== 'production';
 
   return {
-    optimization: {
-      minimizer: [
-        new TerserPlugin({cache: true, parallel: true}),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    },
     entry: {
-      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+      app: glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
     },
     output: {
-      filename: 'app.js',
-      path: path.resolve(__dirname, '../priv/static/js')
+      path: path.resolve(__dirname, '../priv/static/js'),
+      filename: '[name].js',
+      publicPath: '/js/'
     },
-    devtool: devMode ? 'eval-cheap-module-source-map' : 'source-map',
     module: {
       rules: [
         {
@@ -43,7 +35,11 @@ module.exports = (_env, options) => {
     },
     plugins: [
       new MiniCssExtractPlugin({filename: '../css/app.css'}),
-      new CopyWebpackPlugin({patterns: [{from: 'static/', to: '../'}]})
-    ]
+      new CopyPlugin({patterns: [{from: 'static/', to: '../'}]})
+    ],
+    optimization: {
+      minimizer: [new CssMinimizerPlugin()]
+    },
+    devtool: devMode ? 'eval-cheap-module-source-map' : 'source-map'
   };
 };
