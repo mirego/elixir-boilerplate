ecb96fe081538142fe72774a35f5c78ae9eec496
Guillaume Cauchon <gcauchon@gmail.com>
2022-04-25 11:40:06 -0400

    Extract the helpers module out of runtime.exs (#172)
    * Extract the helpers module out of runtime.exs
    * Remove useless pipe in parse_env/2
    * Add missing DATABASE_POOL_SIZE in .env.test
    * Rename ConfigHelpers to Config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,99 +1,42 @@
 import Config
+import ElixirBoilerplate.Config
 
-defmodule Environment do
-  @moduledoc """
-  This modules provides various helpers to handle environment metadata
-  """
-
-  def get(key), do: System.get_env(key)
-
-  def get_boolean(key) do
-    case get(key) do
-      "true" -> true
-      "1" -> true
-      _ -> false
-    end
-  end
-
-  def get_integer(key) do
-    case get(key) do
-      value when is_bitstring(value) -> String.to_integer(value)
-      _ -> nil
-    end
-  end
-
-  def get_cors_origins do
-    case Environment.get("CORS_ALLOWED_ORIGINS") do
-      origins when is_bitstring(origins) ->
-        origins
-        |> String.split(",")
-        |> case do
-          [origin] -> origin
-          origins -> origins
-        end
-
-      _ ->
-        nil
-    end
-  end
-
-  def get_endpoint_url_config(nil), do: nil
-  def get_endpoint_url_config(""), do: nil
-
-  def get_endpoint_url_config(uri) do
-    [
-      host: uri.host,
-      scheme: uri.scheme,
-      port: uri.port
-    ]
-  end
-
-  def get_uri_part(%URI{host: host}, :host), do: host
-  def get_uri_part(%URI{port: port}, :port), do: port
-  def get_uri_part(%URI{scheme: scheme}, :scheme), do: scheme
-  def get_uri_part(_, _), do: nil
-
-  def get_safe_uri(nil), do: nil
-  def get_safe_uri(""), do: nil
-  def get_safe_uri(url), do: URI.parse(url)
-end
-
-canonical_uri = Environment.get_safe_uri(Environment.get("CANONICAL_URL"))
-static_uri = Environment.get_safe_uri(Environment.get("STATIC_URL"))
+canonical_uri = get_env("CANONICAL_URL", :uri)
+static_uri = get_env("STATIC_URL", :uri)
 
 config :elixir_boilerplate,
-  canonical_host: Environment.get_uri_part(canonical_uri, :host),
-  force_ssl: Environment.get_uri_part(canonical_uri, :scheme) == "https"
+  canonical_host: get_uri_part(canonical_uri, :host),
+  force_ssl: get_uri_part(canonical_uri, :scheme) == "https"
 
 config :elixir_boilerplate, ElixirBoilerplate.Repo,
-  pool_size: Environment.get_integer("DATABASE_POOL_SIZE"),
-  ssl: Environment.get_boolean("DATABASE_SSL"),
-  url: Environment.get("DATABASE_URL")
+  url: get_env!("DATABASE_URL"),
+  ssl: get_env("DATABASE_SSL", :boolean),
+  pool_size: get_env!("DATABASE_POOL_SIZE", :integer)
 
 config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
-  debug_errors: Environment.get_boolean("DEBUG_ERRORS"),
-  http: [port: Environment.get("PORT")],
-  secret_key_base: Environment.get("SECRET_KEY_BASE"),
-  static_url: Environment.get_endpoint_url_config(static_uri),
-  url: Environment.get_endpoint_url_config(canonical_uri)
+  http: [port: get_env!("PORT", :integer)],
+  secret_key_base: get_env!("SECRET_KEY_BASE"),
+  url: get_endpoint_url_config(canonical_uri),
+  static_url: get_endpoint_url_config(static_uri),
+  debug_errors: get_env("DEBUG_ERRORS", :boolean)
 
 config :elixir_boilerplate, ElixirBoilerplateWeb.Router,
-  session_key: Environment.get("SESSION_KEY"),
-  session_signing_salt: Environment.get("SESSION_SIGNING_SALT")
+  session_key: get_env!("SESSION_KEY"),
+  session_signing_salt: get_env!("SESSION_SIGNING_SALT")
 
-config :elixir_boilerplate, Corsica, origins: Environment.get_cors_origins()
+config :elixir_boilerplate, Corsica, origins: get_env("CORS_ALLOWED_ORIGINS", :cors)
 
 config :elixir_boilerplate,
   basic_auth: [
-    username: Environment.get("BASIC_AUTH_USERNAME"),
-    password: Environment.get("BASIC_AUTH_PASSWORD")
+    username: get_env("BASIC_AUTH_USERNAME"),
+    password: get_env("BASIC_AUTH_PASSWORD")
   ]
 
 config :sentry,
-  dsn: Environment.get("SENTRY_DSN"),
-  environment_name: Environment.get("SENTRY_ENVIRONMENT_NAME"),
-  included_environments: [Environment.get("SENTRY_ENVIRONMENT_NAME")]
+  dsn: get_env("SENTRY_DSN"),
+  environment_name: get_env("SENTRY_ENVIRONMENT_NAME"),
+  included_environments: [get_env("SENTRY_ENVIRONMENT_NAME")]
 
 config :new_relic_agent,
-  app_name: System.get_env("NEW_RELIC_APP_NAME"),
-  license_key: System.get_env("NEW_RELIC_LICENSE_KEY")
+  app_name: get_env("NEW_RELIC_APP_NAME"),
+  license_key: get_env("NEW_RELIC_LICENSE_KEY")
