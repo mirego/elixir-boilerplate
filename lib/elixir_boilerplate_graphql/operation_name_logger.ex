38c0618ddc3dcccd09a22d5c0de14ec6caaef114
Rémi Prévost <rprevost@mirego.com>
2022-10-06 13:03:20 -0400

    Add OperationNameLogger Absinthe middleware (#181)

---
+++ lib/elixir_boilerplate_graphql/operation_name_logger.ex
@@ -0,0 +1,20 @@
+defmodule ElixirBoilerplateGraphQL.OperationNameLogger do
+  @behaviour Absinthe.Middleware
+
+  alias Absinthe.Blueprint.Document.Operation
+
+  def call(resolution, _opts) do
+    case Enum.find(resolution.path, &current_operation?/1) do
+      %Operation{name: name} when not is_nil(name) ->
+        Logger.metadata(graphql_operation_name: name)
+
+      _ ->
+        Logger.metadata(graphql_operation_name: "#NULL")
+    end
+
+    resolution
+  end
+
+  defp current_operation?(%Operation{current: true}), do: true
+  defp current_operation?(_), do: false
+end
