511898608c79648f822644ff7792c2db75bc9ee3
Rémi Prévost <rprevost@mirego.com>
2022-10-06 10:17:43 -0400

    Ah!

--- lib/elixir_boilerplate_graphql/operation_name_logger.ex
+++ lib/elixir_boilerplate_graphql/operation_name_logger.ex
@@ -1,14 +1,18 @@
 defmodule ElixirBoilerplateGraphQL.OperationNameLogger do
   @behaviour Absinthe.Middleware
 
+  alias Absinthe.Blueprint.Document.Operation
+
   def call(resolution, _opts) do
-    with %Absinthe.Blueprint.Document.Operation{name: name} <- Enum.find(resolution.path, &current_operation?/1) do
+    with %Operation{name: name} when not is_nil(name) <- Enum.find(resolution.path, &current_operation?/1) do
       Logger.metadata(graphql_operation_name: name)
+    else
+      _ -> Logger.metadata(graphql_operation_name: "#NULL")
     end
 
     resolution
   end
 
-  defp current_operation?(%Absinthe.Blueprint.Document.Operation{current: true}), do: true
+  defp current_operation?(%Operation{current: true}), do: true
   defp current_operation?(_), do: false
 end
