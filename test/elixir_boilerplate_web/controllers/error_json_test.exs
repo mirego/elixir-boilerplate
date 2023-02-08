05374bf8f37d8a3b005c4c1f229b440f40dfff39
Guillaume Cauchon <gcauchon@gmail.com>
2023-02-07 21:17:30 -0500

    Fix tests

---
+++ test/elixir_boilerplate_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule ElixirBoilerplateWeb.ErrorJSONTest do
+  use ElixirBoilerplateWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert ElixirBoilerplateWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert ElixirBoilerplateWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -5,42 +5,35 @@ defmodule ElixirBoilerplateWeb.ConnCase do
 
   Such tests rely on `Phoenix.ConnTest` and also
   import other functionality to make it easier
-  to build common datastructures and query the data layer.
+  to build common data structures and query the data layer.
 
   Finally, if the test case interacts with the database,
-  it cannot be async. For this reason, every test runs
-  inside a transaction which is reset at the beginning
-  of the test unless the test case is marked as async.
+  we enable the SQL sandbox, so changes done to the database
+  are reverted at the end of every test. If you are using
+  PostgreSQL, you can even run database tests asynchronously
+  by setting `use ElixirBoilerplateWeb.ConnCase, async: true`, although
+  this option is not recommended for other databases.
   """
 
   use ExUnit.CaseTemplate
 
-  alias Ecto.Adapters.SQL.Sandbox
-  alias ElixirBoilerplate.Repo
-  alias ElixirBoilerplateWeb.Endpoint
-  alias Phoenix.ConnTest
-
   using do
     quote do
+      # The default endpoint for testing
+      @endpoint ElixirBoilerplateWeb.Endpoint
+
+      use ElixirBoilerplateWeb, :verified_routes
+
       # Import conveniences for testing with connections
       import Plug.Conn
       import Phoenix.ConnTest
-
-      import ElixirBoilerplateWeb.Router.Helpers
-
-      # The default endpoint for testing
-      @endpoint Endpoint
+      import ElixirBoilerplateWeb.ConnCase
     end
   end
 
   setup tags do
-    :ok = Sandbox.checkout(Repo)
-
-    unless tags[:async] do
-      Sandbox.mode(Repo, {:shared, self()})
-    end
-
-    {:ok, conn: %{ConnTest.build_conn() | host: host()}}
+    ElixirBoilerplate.DataCase.setup_sandbox(tags)
+    {:ok, conn: %{Phoenix.ConnTest.build_conn() | host: host()}}
   end
 
   defp host, do: Application.get_env(:elixir_boilerplate, :canonical_host)
