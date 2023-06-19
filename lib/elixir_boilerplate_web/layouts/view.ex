35be4923ff37b987d4633b72b1c211342e7bcfb0
Guillaume Cauchon <gcauchon@gmail.com>
2023-06-19 11:03:15 -0400

    Move flash handling to the new Phoenix.Flash module

--- lib/elixir_boilerplate_web/layouts/view.ex
+++ lib/elixir_boilerplate_web/layouts/view.ex
@@ -2,7 +2,7 @@ defmodule ElixirBoilerplateWeb.Layouts.View do
   use Phoenix.View, root: "lib/elixir_boilerplate_web", path: "layouts/templates", namespace: ElixirBoilerplateWeb
   use Phoenix.HTML
 
-  import Phoenix.Controller, only: [get_flash: 2]
+  import Phoenix.Flash, only: [get: 2]
 
   alias ElixirBoilerplateWeb.Router.Helpers, as: Routes
 end
