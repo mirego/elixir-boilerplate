47ba7935ad1e14f265f9d160949f0b9c0f17008d
Guillaume Cauchon <gcauchon@gmail.com>
2023-09-07 13:44:19 -0400

    Comment heroicons configuration from Tailwind for now

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -4,8 +4,8 @@
 // https://tailwindcss.com/docs/configuration
 
 const plugin = require('tailwindcss/plugin');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 module.exports = {
   content: ['./js/**/*.js', '../lib/*_web.ex', '../lib/*_web/**/*.*ex'],
