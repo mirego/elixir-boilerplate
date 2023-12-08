6a4173ac3f106fc68a069905b0fb84191020e9ff
Matthew Kevork <35671881+matkev@users.noreply.github.com>
2023-12-08 10:22:53 -0500

    Restrict graphql content types (#458)
    * include subdomains in hsts rule
    
    * handle web and graphql content-types separately
    
    * revert hsts include subdomains

--- lib/elixir_boilerplate_graphql/router.ex
+++ lib/elixir_boilerplate_graphql/router.ex
@@ -5,6 +5,13 @@ defmodule ElixirBoilerplateGraphQL.Router do
     @moduledoc false
     use Plug.Router
 
+    plug(
+      Plug.Parsers,
+      parsers: [:json],
+      pass: [],
+      json_decoder: Phoenix.json_library()
+    )
+
     plug(:match)
     plug(:dispatch)
 
