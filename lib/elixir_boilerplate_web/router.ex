2c9186ce3516570a260839cee34e721b14e6b0bb
Simon Prévost <sprevost@mirego.com>
2025-08-28 10:45:17 -0400

    Remove telemetry_ui_allowed

--- lib/elixir_boilerplate_web/router.ex
+++ lib/elixir_boilerplate_web/router.ex
@@ -23,7 +23,7 @@ defmodule ElixirBoilerplateWeb.Router do
     #
     # Metrics can contains sensitive data you should protect it under authorization
     # See https://github.com/mirego/telemetry_ui#security
-    get("/metrics", TelemetryUI.Web, [], assigns: %{telemetry_ui_allowed: true})
+    get("/metrics", TelemetryUI.Web, [], assigns: %{telemetry_ui_allowed: false})
   end
 
   scope "/", ElixirBoilerplateWeb do
